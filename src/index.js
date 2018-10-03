6a454a24362f69912129557c0bbe4a1ce502b582
Ivan Shelest <ishelest2012@gmail.com>
2018-10-03 11:34:41 +0300

    feature: implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
-}
+
+	let tempBase = base;
+	let countOfZeros;
+  for (let i = 2; i <= base; i++) {
+    if (tempBase % i === 0) {
+			
+			let pow = 0;
+      do {
+				tempBase /= i;     
+				pow++;
+			} while (tempBase % i === 0)
+			
+			let count = 0;
+			let tempNumber = number;
+      while (tempNumber > 0) {
+				tempNumber = Math.floor(tempNumber / i);
+				count += tempNumber; 
+      }
+      countOfZeros = Math.floor(count / pow)
+    }
+  }
+  return countOfZeros;
+	
+};
+
+
+
+
+
